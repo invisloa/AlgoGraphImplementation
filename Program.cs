bd7681abdc416e0c61a3b3c20a9f3d7ba30e1dab
invisloa <91217120+invisloa@users.noreply.github.com>
2023-04-30 15:28:35 +0200

    use GraphNode class

--- Program.cs
+++ Program.cs
@@ -138,13 +138,20 @@ public class Program
 		graph.AddEdge(1, 2);
 		graph.AddEdge(1, 3);
 		graph.AddEdge(2, 4);
-		graph.AddEdge(2, 6);
+	//	graph.AddEdge(2, 6);
 		graph.AddEdge(3, 5);
 		graph.AddEdge(6, 5);
+		graph.AddEdge(4, 8);
+		graph.AddEdge(4, 9);
+		graph.AddEdge(6, 11);
+		graph.AddEdge(6, 10);
+		graph.RemoveEdge(2, 4);
+
+
 
 		// Wykonanie przeszukiwania wszerz grafu od wierzchołka 1
 		Console.WriteLine("BFS traversal:");
-		var bfs = graph.BFS(1);
+		var bfs = graph.BFS(4);
 		foreach (var node in bfs)
 		{
 			Console.Write(node + " ");
@@ -153,7 +160,7 @@ public class Program
 
 		// Wykonanie przeszukiwania w głąb grafu od wierzchołka 1
 		Console.WriteLine("DFS traversal:");
-		var dfs = graph.DFS(1);
+		var dfs = graph.DFS(3);
 		foreach (var node in dfs)
 		{
 			Console.Write(node + " ");
